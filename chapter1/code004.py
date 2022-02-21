9c954b43ba62d0bccef2cbbfd1193b64355c6b19
LAPTOP-0D8CP5Q4\plumn <plumnix@outlook.com>
2022-02-21 12:00:55 +0800

    1. add code 004.

---
+++ chapter1/code004.py
@@ -0,0 +1,78 @@
+# 4. 寻找两个正序数组的中位数
+
+# 给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的中位数 。
+#
+# 算法的时间复杂度应该为 O(log (m+n)) 。
+#
+# 示例 1：
+#
+# 输入：nums1 = [1,3], nums2 = [2]
+# 输出：2.00000
+# 解释：合并数组 = [1,2,3] ，中位数 2
+
+# 示例 2：
+#
+# 输入：nums1 = [1,2], nums2 = [3,4]
+# 输出：2.50000
+# 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+#
+# 提示：
+#
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -106 <= nums1[i], nums2[i] <= 106
+
+from typing import List
+
+
+class Solution:
+
+    @staticmethod
+    def find_median_sorted_arrays(nums1: List[int], nums2: List[int]) -> float:
+        def get_kth_element(k):
+            """
+            - 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+            - 这里的 "/" 表示整除
+            - nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+            - nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+            - 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+            - 这样 pivot 本身最大也只能是第 k-1 小的元素
+            - 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+            - 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+            - 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+            """
+
+            index1, index2 = 0, 0
+            while True:
+                # 特殊情况
+                if index1 == m:
+                    return nums2[index2 + k - 1]
+                if index2 == n:
+                    return nums1[index1 + k - 1]
+                if k == 1:
+                    return min(nums1[index1], nums2[index2])
+
+                # 正常情况
+                new_index1 = min(index1 + k // 2 - 1, m - 1)
+                new_index2 = min(index2 + k // 2 - 1, n - 1)
+                pivot1, pivot2 = nums1[new_index1], nums2[new_index2]
+                if pivot1 <= pivot2:
+                    k -= new_index1 - index1 + 1
+                    index1 = new_index1 + 1
+                else:
+                    k -= new_index2 - index2 + 1
+                    index2 = new_index2 + 1
+
+        m, n = len(nums1), len(nums2)
+        total_length = m + n
+        if total_length % 2 == 1:
+            return get_kth_element((total_length + 1) // 2)
+        else:
+            return (get_kth_element(total_length // 2) + get_kth_element(total_length // 2 + 1)) / 2
+
+
+if __name__ == "__main__":
+    print(Solution().find_median_sorted_arrays([0, 1, 2], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
